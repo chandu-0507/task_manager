46f283bacd197716af049e97e46df0e62046ddc8
chandu-0507 <chanduummanaveni2004@gmail.com>
2026-03-31 16:52:15 +0530

    Add automatic superuser creation

---
+++ tasks/management/commands/createadmin.py
@@ -0,0 +1,23 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+import os
+
+
+class Command(BaseCommand):
+    help = 'Create admin user if it does not exist'
+
+    def handle(self, *args, **kwargs):
+        username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+        email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+        password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+
+        if not username or not email or not password:
+            self.stdout.write(self.style.WARNING('Superuser environment variables not set.'))
+            return
+
+        if User.objects.filter(username=username).exists():
+            self.stdout.write(self.style.WARNING('Superuser already exists.'))
+            return
+
+        User.objects.create_superuser(username=username, email=email, password=password)
+        self.stdout.write(self.style.SUCCESS('Superuser created successfully.'))
